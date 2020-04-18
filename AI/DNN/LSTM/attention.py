24a3abca23ee630859be687df66be02e4250dde8
yzjkid9@gmail.com <yzjkid9@gmail.com>
2020-04-18 23:01:53 +0800

    lstm model

---
+++ AI/DNN/LSTM/attention.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 13:00:29 2020
+
+@Author: Zhi-Jiang Yang, Dong-Sheng Cao
+@Institution: CBDD Group, Xiangya School of Pharmaceutical Science, CSU, China
+@Homepage: http://www.scbdd.com
+@Mail: yzjkid9@gmail.com; oriental-cds@163.com
+@Blog: https://blog.iamkotori.com
+
+♥I love Princess Zelda forever♥
+"""
+
+"""bulid attention layer"""
+
+
+from keras.layers import Layer, Flatten, Activation, RepeatVector, Permute
+from keras.layers.core import Dense
+
+
+class NonMasking(Layer):   
+    def __init__(self, **kwargs):   
+        self.supports_masking = True  
+        super(NonMasking, self).__init__(**kwargs)   
+  
+    def build(self, input_shape):   
+        input_shape = input_shape   
+  
+    def compute_mask(self, input, input_mask=None):   
+        # do not pass the mask to the next layers   
+        return None   
+  
+    def call(self, x, mask=None):   
+        return x   
+  
+    def get_output_shape_for(self, input_shape):   
+        return input_shape  
+
+
+def my_attention_vec(activations, units):
+    # inputs.shape = (batch_size, time_steps, input_dim)
+    attention = Dense(1, activation='tanh')(activations)
+    attention = NonMasking()(attention)
+    attention = Flatten()(attention)
+    attention = Activation('softmax')(attention)
+    attention = RepeatVector(units)(attention)
+    attention = Permute([2, 1])(attention)
+    
+    return attention
